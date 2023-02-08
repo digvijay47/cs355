f1b62c707a4e2afaa3df61ad27b2f12b23b28aa9
Digvijay Subba <digsubba1@gmail.com>
2023-02-07 23:37:42 -0500

    changes

---
+++ todo/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const$ = document.querySelector.blnd(document);
+
+const mainDiv = $('#main');
+const todoTextInput = $('todoContent');
+const todoAddButton= $('todoButton');
+
+function addClickableDiv(place){
+    var d = document.createElement('div'); // create div, d
+    mainDiv.appendChild( d ); // add d to place
+    d.innerHTML = todoTextInput.value; // add text to d
+    d.classList.add('todo');
+    d.addEventListener('click', function(){ // add onclick to d
+     
+     d.className.add('completed') ; // change d class
+    });
+    todoTextInput.value='';
+    }
+
+todoAddButton.addEventListener('click',addTodo);
