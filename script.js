94a8f4b74163d3f902c5d61a36167fba60156051
Digvijay Subba <digsubba1@gmail.com>
2023-02-14 18:41:33 -0500

    some changes

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ const $= document.querySelector.bind(document);
 
 //const clckBtn= document.getElementById('clickmebutton')
 const clckBtn= $('#clickmebutton');
+const change = $('.super');
+const imageONe = $('img');
 
 clckBtn.addEventListener('click',foo);
 function foo(){
     alert('helloooo')
+    change.style.fontSize = 30;
+    change.style.backgroundColor = 'green';
 }
+
+function imageChange(){
+    imageONe.style.width =600;
+}
+
+
+function imageChangeTwo(){
+    imageONe.style.width =300;
+}
+imageONe.addEventListener('mouseover', imageChange);
+imageONe.addEventListener('mouseleave', imageChangeTwo);
