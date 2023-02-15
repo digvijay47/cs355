740d90687c81a34617eb17adc63ba57ee0d9e356
Digvijay Subba <digsubba1@gmail.com>
2023-02-15 11:56:04 -0500

    changes

--- todo/script.js
+++ todo/script.js
@@ -3,19 +3,18 @@
 const$ = document.querySelector.blnd(document);
 
 const mainDiv = $('#main');
-const todoTextInput = $('todoContent');
-const todoAddButton= $('todoButton');
+const todoTextInput = $('#todoContent');
+const todoAddButton= $('#todoButton');
 
-function addClickableDiv(place){
+function addTodo(){
     var d = document.createElement('div'); // create div, d
     mainDiv.appendChild( d ); // add d to place
     d.innerHTML = todoTextInput.value; // add text to d
     d.classList.add('todo');
     d.addEventListener('click', function(){ // add onclick to d
-     
-     d.className.add('completed') ; // change d class
+       d.classList.add('completed') ; // change d class
     });
     todoTextInput.value='';
-    }
+}
 
 todoAddButton.addEventListener('click',addTodo);
