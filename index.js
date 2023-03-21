b86f76bbcb3d2d0cfc9a54f34ab4402a28e60565
Digvijay Subba <digsubba1@gmail.com>
2023-03-21 19:27:11 -0400

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 app = express(); //Initializing the express
-//app.use(cors());
+app.use(cors());
 app.use(express.static("todo"));
 
 let count =0;
